1af1e4ae4a41f47828e71e927fafe181d616f2bc
Bogdan Orzea <bogdan.orzea@gmail.com>
2017-05-25 00:56:25 +0300

    Clean-up and extracted some resources

--- app/src/main/java/com/bogdanorzea/mockmusicplayer/NowPlayingActivity.java
+++ app/src/main/java/com/bogdanorzea/mockmusicplayer/NowPlayingActivity.java
@@ -10,52 +10,44 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class NowPlayingActivity extends AppCompatActivity {
-    TextView artistName;
-    TextView songName;
-
-    ImageButton previousButton;
-    ImageButton rewindButton;
-    ImageButton playButton;
-    ImageButton forwardButton;
-    ImageButton skipButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_playing);
 
-        artistName = (TextView) findViewById(R.id.artist_name);
-        songName = (TextView) findViewById(R.id.song_name);
+        TextView artistName = (TextView) findViewById(R.id.artist_name);
+        TextView songName = (TextView) findViewById(R.id.song_name);
 
-        previousButton = (ImageButton) findViewById(R.id.previous);
+        ImageButton previousButton = (ImageButton) findViewById(R.id.previous);
         previousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(NowPlayingActivity.this, "Jump to previous song", Toast.LENGTH_SHORT).show();
             }
         });
-        rewindButton = (ImageButton) findViewById(R.id.rewind);
+        ImageButton rewindButton = (ImageButton) findViewById(R.id.rewind);
         rewindButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(NowPlayingActivity.this, "Rewind 5 seconds", Toast.LENGTH_SHORT).show();
             }
         });
-        playButton = (ImageButton) findViewById(R.id.play);
+        ImageButton playButton = (ImageButton) findViewById(R.id.play);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(NowPlayingActivity.this, "Play/pause the sound", Toast.LENGTH_SHORT).show();
             }
         });
-        forwardButton = (ImageButton) findViewById(R.id.forward);
+        ImageButton forwardButton = (ImageButton) findViewById(R.id.forward);
         forwardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(NowPlayingActivity.this, "Forward 5 seconds", Toast.LENGTH_SHORT).show();
             }
         });
-        skipButton = (ImageButton) findViewById(R.id.skip);
+        ImageButton skipButton = (ImageButton) findViewById(R.id.skip);
         skipButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/bogdanorzea/mockmusicplayer/SettingsActivity.java
+++ app/src/main/java/com/bogdanorzea/mockmusicplayer/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.bogdanorzea.mockmusicplayer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.SleepAfter, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
